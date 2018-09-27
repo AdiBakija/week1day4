30978aaacc36c50be93acada51f7b1449a6dd723
AdiBakija <adibakija35@hotmail.com>
2018-09-27 17:58:14 +0000

    Final submission for pythagorean assignment

--- anonFuncArrIter/pythagoreanCalc.js
+++ anonFuncArrIter/pythagoreanCalc.js
@@ -4,9 +4,15 @@ var input = [
   { x: 8, y: 15 }
 ];
 
-var one = input[0]
-var result = input.map(/* Your code here */);
+
+//console.log(oneX, oneY, result);
+
+var result = input.map(function (num) {
+    var resultFinal = Math.sqrt(Math.pow(num.x, 2) + Math.pow(num.y, 2));
+  return resultFinal;
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
 console.log(result[2] === 17);
+console.log(result);
