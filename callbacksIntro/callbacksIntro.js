1ece80f711be297097200967b3d4af0360972a83
AdiBakija <adibakija35@hotmail.com>
2018-09-27 16:02:03 +0000

    Refactored for loop within findWaldo to include .forEach() method

--- callbacksIntro/callbacksIntro.js
+++ callbacksIntro/callbacksIntro.js
@@ -1,9 +1,9 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(element) {
+  if (element === "Waldo") {
       found(arr);   // execute callback
     }
-  }
+});
 }
 
 function actionWhenFound(arr) {
@@ -20,3 +20,16 @@ findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 /*Modify the callback function in the previous example so that logs the index of the array
 where Waldo is found, ie. "Found Waldo at index 2!".
 (You will need to modify actionWhenFound to receive the index.*/
+
+// var array1 = ['a', 'b', 'c'];
+
+// arr.forEach(function(element) {
+//   if (element === "Waldo") {
+//       found(arr);   // execute callback
+//     }
+// });
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(arr);   // execute callback
+//     }
+//   }
