4ffa9a86e3824027a29d34461421b0fc40cc58be
AdiBakija <adibakija35@hotmail.com>
2018-09-27 20:40:10 +0000

    Updated code to include explanations and removed unneccessary code

--- customSorting/customSorting.js
+++ customSorting/customSorting.js
@@ -5,12 +5,12 @@ var students = [
   { id: 4, name: "alex",     age: 30 }
 ];
 
-//Sorting by age
+//This is a callback function that can be passed to sort() with the updated array.
 function ageSort(a, b){
   var nameA=a.name, nameB=b.name
-  if (nameA === nameB) {
+  if (nameA === nameB) {//This sorts the array by age and modifies
     return b.age-a.age
-  } else if (nameA < nameB) {//This resorts the array
+  } else if (nameA < nameB) {//This re-sorts and re-modifies the array by name
     return -1;
   } else if (nameA > nameB) {
     return 1;
@@ -20,14 +20,3 @@ function ageSort(a, b){
 }
 
 console.log(students.sort(ageSort));
-
-
-//Expected output below --------------
-
-/*var students = [
-  { id: 4, name: "alex",     age: 30 },
-  { id: 3, name: "alex",     age: 22 },
-  { id: 1, name: "bruce",    age: 40 },
-  { id: 2, name: "zoidberg", age: 22 },
-
-];*/
